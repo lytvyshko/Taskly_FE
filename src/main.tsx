9da2ca2ae0770c58b30de8aaa93052e1aa437abf
Pavlo Lytvyshko <pavlo.lytvyshko@jointoit.com>
2026-07-13 21:37:27 +0300

    add basic configuration and npm packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { theme } from './theme/theme';
+import App from './App.tsx';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
-  </StrictMode>,
-)
+  </ThemeProvider>,
+);
